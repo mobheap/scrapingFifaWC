f16aed2279b193b23871509d55931957c3cb745e
mobheap <yassabb27@gmail.com>
2024-07-04 15:47:30 +0100

    added error checking

--- fifa.py
+++ fifa.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-
 years = [i for i in range(1930, 2025, 4)]       #remember that there were no world cups during 1942 and 1946
 years.remove(1942)
 years.remove(1946)
@@ -10,8 +9,13 @@ years.remove(1946)
 def get_matches(year):
     website = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
     response = requests.get(website)
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve data for year {year}")
+        return pd.DataFrame()
+    
     content = response.text
-
+    
     soup = BeautifulSoup(content, 'lxml')
 
     matches = soup.find_all('div', class_='footballbox')
@@ -26,9 +30,9 @@ def get_matches(year):
         away_team = match.find('th', class_='faway')
         
         if home_team and score_result and away_team:
-            home.append(home_team.get_text())
-            score.append(score_result.get_text())
-            away.append(away_team.get_text())
+            home.append(home_team.get_text().strip())
+            score.append(score_result.get_text().strip())
+            away.append(away_team.get_text().strip())
 
     matches_dict = {'home': home, 'score': score, 'away': away}
     football_df = pd.DataFrame(matches_dict)
